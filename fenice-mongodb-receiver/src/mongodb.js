c85dc30a84ae48c916c1e3561b9096793f1dc979
Eugeniovinicio Berretta <euberdeveloper@gmail.com>
2019-07-10 18:09:25 +0200

    added bson (not tested yet)

--- fenice-mongodb-receiver/src/mongodb.js
+++ fenice-mongodb-receiver/src/mongodb.js
@@ -1,22 +1,22 @@
-var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectID;
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
 
-var config = require('./config/config.json');
+const config = require('./config/config.json');
 
 // MongoDB config
-var address = config.mongodb.address;
-var port = config.mongodb.port;
-var dbName = config.mongodb.dbName;
-var rawDataCollection = config.mongodb.collections[0];
+const address = config.mongodb.address;
+const port = config.mongodb.port;
+const dbName = config.mongodb.dbName;
+const rawDataCollection = config.mongodb.collections[0];
 
-var url = "mongodb://" + address + ":" + port + "/" + dbName;
+const url = "mongodb://" + address + ":" + port + "/" + dbName;
 
 function insertDocumentInCollection(myobj, collection) {
     try {
-        MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+        MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
             if (err) throw err;
-            var dbo = db.db(dbName);
-            dbo.collection(collection).insertOne(myobj, function(err, res) {
+            const dbo = db.db(dbName);
+            dbo.collection(collection).insertOne(myobj, function (err, _res) {
                 if (err) throw err;
                 db.close();
             });
@@ -29,12 +29,12 @@ function insertDocumentInCollection(myobj, collection) {
 var self = module.exports = {
 
     /*Function to find a value in a Collection*/
-    findInCollection: function(_id, collection, callback) {
+    findInCollection: function (_id, collection, callback) {
         try {
-            MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+            MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
                 if (err) throw err;
                 var dbo = db.db(dbName);
-                dbo.collection(collection).find({ _id: ObjectId(_id) }).toArray(function(err, result) {
+                dbo.collection(collection).find({ _id: ObjectId(_id) }).toArray(function (err, result) {
                     if (err) throw err;
                     db.close();
                     callback(result);
@@ -46,13 +46,13 @@ var self = module.exports = {
     },
 
     /*Function to find "x" last values*/
-    findInCollectionLast: function(numOfData, callback) {
+    findInCollectionLast: function (numOfData, callback) {
         numOfData = parseInt(numOfData);
         try {
-            MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+            MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
                 if (err) throw err;
                 var dbo = db.db(dbName);
-                dbo.collection(rawDataCollection).find({}).sort({ _id: -1 }).limit(numOfData).toArray(function(err, result) {
+                dbo.collection(rawDataCollection).find({}).sort({ _id: -1 }).limit(numOfData).toArray(function (err, result) {
                     if (err) throw err;
                     db.close();
                     callback(result);
@@ -64,17 +64,17 @@ var self = module.exports = {
     },
 
     /*Function to add a value in a Collection*/
-    insertData: function(obj) {
+    insertData: function (obj) {
         insertDocumentInCollection(obj, rawDataCollection);
     },
 
     /*Function to find a value in a Collection*/
-    findRawData: function(callback) {
+    findRawData: function (callback) {
         try {
-            MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+            MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
                 if (err) throw err;
                 var dbo = db.db(dbName);
-                dbo.collection(collection).toArray(function(err, result) {
+                dbo.collection(collection).toArray(function (err, result) {
                     if (err) throw err;
                     db.close();
                     callback(result);
---
+++ fenice-mongodb-receiver/src/receiver.js
@@ -0,0 +1,45 @@
+const mqtt = require('mqtt');
+const os = require('os');
+const { deserialize } = require('bson');
+
+const mongo = require('./mongodb.js');
+const config = require('./config/config.json');
+
+const topic = config.mqtt.topic[0];
+const hostname = config.mqtt.hostname;
+const port = config.mqtt.port;
+const mqttUri = 'mqtt://' + hostname + ':' + port;
+
+const client = mqtt.connect(mqttUri);
+
+console.log(" OS: " + os.type() + " " + os.release() + " (" + os.arch() + ")");
+console.log("RAM: " + os.totalmem() / 1048576 + " MB (total), " + os.freemem() / 1048576 + " MB (free)");
+console.log("CPU: " + os.cpus()[0].speed + " MHz " + os.cpus()[0].model + "\n");
+
+client.on('connect', () => {
+    console.log('Connecting to ' + topic + '...')
+    client.subscribe(topic, function (err) {
+        if (err) {
+            console.error('Error in connecting ', err);
+        }
+        else {
+            console.log('Connected!');
+        }
+    });
+
+});
+
+client.on('offline', () => {
+    client.unsubscribe(topic);
+    console.log('Disconnected from /' + topic + '.');
+});
+
+client.on('message', function (_topic, message) {
+    try {
+        const json = JSON.parse(deserialize(message));
+        // console.log(json);
+        mongo.insertData(json);
+    } catch (error) {
+        console.log('json bad format', error, message)
+    }
+});
